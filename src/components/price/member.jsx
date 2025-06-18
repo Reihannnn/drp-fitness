0c52e79a494eca2984ef7601fcec5baab8f92043
Reihannnn <areihanahmad@gmail.com>
2025-06-18 22:55:08 +0700

    change price squad

--- src/components/price/member.jsx
+++ src/components/price/member.jsx
@@ -2,7 +2,9 @@ const MemberCategory = () => {
   return (
     <div className="bg-gray-900 rounded-2xl p-8 shadow-lg flex flex-col justify-between">
       <div>
-        <h3 className="text-2xl font-semibold mb-4 lg:text-[1.4rem] underline underline-offset-8">Member ( solo )</h3>
+        <h3 className="text-2xl font-semibold mb-4 lg:text-[1.4rem] underline underline-offset-8">
+          Member ( solo )
+        </h3>
         <p className="text-3xl font-bold mb-2">Rp.155.000</p>
         <ul className="space-y-2">
           <li>- Akses full 1 bulan menjadi member</li>
@@ -10,8 +12,6 @@ const MemberCategory = () => {
         </ul>
 
         <br />
-
-        
       </div>
     </div>
   );
@@ -21,8 +21,10 @@ export const MemberCategorySquad = () => {
   return (
     <div className="bg-gray-900 rounded-2xl p-8 shadow-lg flex flex-col justify-between">
       <div>
-        <h3 className="text-2xl font-semibold mb-4 lg:text-[0.846rem] underline underline-offset-8">Member ( Squad / 4 orang)</h3>
-        <p className="text-3xl font-bold mb-2">Rp.500.000</p>
+        <h3 className="text-2xl font-semibold mb-4 lg:text-[0.846rem] underline underline-offset-8">
+          Member ( Squad / 4 orang)
+        </h3>
+        <p className="text-3xl font-bold mb-2">Rp.520.000</p>
         <ul className="space-y-2">
           <li>- Akses full 1 bulan menjadi member</li>
           <li>- Connected to komunitas yang solid, satu tujuan</li>
