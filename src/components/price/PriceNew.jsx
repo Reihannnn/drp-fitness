d0a0abdb64c5b9a40169eca71516266dfe129e78
Reihannnn <areihanahmad@gmail.com>
2026-04-23 12:57:51 +0700

    change new price

--- src/components/price/PriceNew.jsx
+++ src/components/price/PriceNew.jsx
@@ -1,9 +1,9 @@
 import { Senam, CardPrice, Visit, Boxing } from "./CardPriceMember";
-import priceList from "/image/pricelist-drp.webp";
+import priceList from "/image/pricelist2-drp.jpeg";
 
 const PriceNew = () => {
   return (
-    <section className=" bg-gray-200 p-16 flex justify-center">
+    <section id="price" className=" bg-gray-200 p-10 lg:p-16 flex justify-center">
       <img src={priceList} alt="daftar harga drp fitness gym cibitung" className="md:w-[42.5%] w-[95%]"/>
     </section>
   );
